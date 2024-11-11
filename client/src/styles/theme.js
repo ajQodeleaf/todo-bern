3a9ba7c89fdb6a39692c760522810c346007d7e2
Aradhya Jain <ajain@qodeleaf.com>
2024-11-11 12:55:47 +0530

    App default theme chakra provider

---
+++ client/src/styles/theme.js
@@ -0,0 +1,11 @@
+'use client';
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+    colors: {
+        primary: "#ff0000",
+        secondary: "#00ff00",
+    }
+});
+
+export default theme;
