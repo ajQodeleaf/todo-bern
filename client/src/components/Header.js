cd74962208de667e8c4cf599fcf0ad76fd5985ff
Aradhya Jain <ajain@qodeleaf.com>
2024-11-11 12:56:18 +0530

    Header component

---
+++ client/src/components/Header.js
@@ -0,0 +1,74 @@
+// Header.js
+'use client';
+import { Box, Flex, HStack, Button, Spacer } from '@chakra-ui/react';
+import { useRouter } from 'next/navigation';
+import { useWallet } from "../context/WalletContext";
+
+const Header = () => {
+  const router = useRouter();
+  const { isConnected, disconnectWallet } = useWallet();
+
+  return (
+    <Box bg="teal.500" color="white" p={4}>
+      <Flex align="center">
+        <Button
+          onClick={() => router.push('/')}
+          variant="link"
+          color="white"
+          fontSize="lg"
+          fontWeight="bold"
+        >
+          Task Manager
+        </Button>
+
+        <Spacer />
+
+        <HStack spacing={4}>
+          <Button
+            onClick={() => router.push('/view-task')}
+            variant="link"
+            color="white"
+            _hover={{ textDecoration: 'underline' }}
+          >
+            View Task
+          </Button>
+
+          <Button
+            onClick={() => router.push('/all-tasks')}
+            variant="link"
+            color="white"
+            _hover={{ textDecoration: 'underline' }}
+          >
+            All Tasks
+          </Button>
+
+          <Button
+            onClick={() => router.push('/delete-task')}
+            variant="link"
+            color="white"
+            _hover={{ textDecoration: 'underline' }}
+          >
+            Delete Task
+          </Button>
+
+          <Button
+            onClick={() => router.push('/update-task')}
+            variant="link"
+            color="white"
+            _hover={{ textDecoration: 'underline' }}
+          >
+            Update Task
+          </Button>
+
+          {isConnected && (
+            <Button onClick={disconnectWallet} colorScheme="red" size="sm">
+              Disconnect
+            </Button>
+          )}
+        </HStack>
+      </Flex>
+    </Box>
+  );
+};
+
+export default Header;
