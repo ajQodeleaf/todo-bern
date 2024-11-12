ee9e7b0bed0f64be3ea07bbd5a69d3c625f1296e
Aradhya Jain <ajain@qodeleaf.com>
2024-11-12 12:26:00 +0530

    Refactor: Add wallet for contract interaction to backend using signer

--- api/contracts/contract.js
+++ api/contracts/contract.js
@@ -3,9 +3,9 @@ const toDoContract = require("../artifacts/contracts/todo.sol/ToDo.json");
 
 const abi = toDoContract.abi;
 const provider = new ethers.JsonRpcProvider(process.env.SEPOLIA_RPC_URL);
-
+const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
 const contractAddress = process.env.TODO_CONTRACT_ADDRESS;
 
-const contract = new ethers.Contract(contractAddress, abi, provider);
+const contract = new ethers.Contract(contractAddress, abi, wallet);
 
 module.exports = { contract };
