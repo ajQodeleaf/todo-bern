4eeb22126d2cc85828615264096abadc447103af
Aradhya Jain <ajain@qodeleaf.com>
2024-11-12 12:26:13 +0530

    Changed port number for API calls

--- api/server.js
+++ api/server.js
@@ -8,7 +8,7 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/ethereum', tasks)
 
-const PORT = 3000;
+const PORT = 4000;
 app.listen(PORT, () => {
     console.log(`Server Running At PORT ${PORT}`)
 })
